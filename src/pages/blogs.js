1ef0275099669062caf05be5c4d81caabb99651c
mohall42 <mike.odane@gmmail.com>
2020-04-26 15:47:47 -0400

    Added cards to blogs page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
+import Container from '@material-ui/core/Container'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
     query{
-        allMarkdownRemark{
-          edges{
-            node{
-              frontmatter{
-                title
-                date
-              }
+      allMarkdownRemark{
+        edges{
+          node{
+            frontmatter{
+              title
+              date
+              description
             }
           }
         }
       }
+    }
     
     
     `)
@@ -24,16 +29,31 @@ const BlogPage = () => {
 
     return (
         <Layout>
+          <Container>
+
             <h1>Blog</h1>
             <ul>
                 {data.allMarkdownRemark.edges.map((edge) => {
+
+                  const post = ({
+                    title: edge.node.frontmatter.title,
+                    date : edge.node.frontmatter.date,
+                    description: edge.node.frontmatter.description
+                  })
                     return (
-                        <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                        </li>
+                        <Card>
+                          <CardMedia>
+
+                          </CardMedia>
+                          <CardContent>
+                            <h1>{post.title}</h1>
+                            <p>{post.date}</p>
+                          </CardContent>
+                        </Card>
                     )
                 })}
             </ul>
+          </Container>
         </Layout>
     )
 
